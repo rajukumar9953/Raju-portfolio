3485f581080e4d86df26bcb5581fe1b41db75b06
Rajukumar <prajapatiraju60417@gmai.com>
2025-06-25 21:58:54 +0530

    Updated index.html with improved styles and fonts

--- gucci project/app.js
+++ gucci project/app.js
@@ -1,28 +1,33 @@
-window.addEventListener('scroll', function () {
-    const bodyLogo = document.getElementById('body-logo');
-    const navLogo = document.getElementById('nav-logo');
-    const a = document.getElementById('box1');
-    const bigimg = document.getElementById('bigimg')
+// window.addEventListener('scroll', function () {
+//     const bodyLogo = document.getElementById('body-logo');
+//     const navLogo = document.getElementById('nav-logo');
+//     const a = document.getElementById('box1');
+//     const bigimg = document.getElementById('bigimg')
 
 
-    const scrollPosition = window.scrollY;
-    if (window.scrollY > 10) {
+//     const scrollPosition = window.scrollY;
+//     if (window.scrollY > 10) {
 
-      bodyLogo.style.zIndex = '99'
-      bodyLogo.style.transition = '0.5s'
-      bodyLogo.style.fontSize = '60px'
-      bodyLogo.style.position = 'fixed'
-      a.style.opacity = 1;
-      bodyLogo.style.marginLeft = '32%'
-      bigimg.style.marginTop = '50px'
+//       bodyLogo.style.zIndex = '99'
+//       bodyLogo.style.transition = '0.5s'
+//       bodyLogo.style.fontSize = '60px'
+//       bodyLogo.style.position = 'fixed'
+//       a.style.opacity = 1;
+//       bodyLogo.style.marginLeft = '32%'
+//       bigimg.style.marginTop = '50px'
 
-      // a.style.transition = "0.4s"
-    } else {
-      a.style.opacity = 0;
-      bodyLogo.style.fontSize = '450px'
-      bodyLogo.style.marginLeft = '10px'
-      bigimg.style.margin = '0'
+//       // a.style.transition = "0.4s"
+//     } else {
+//       a.style.opacity = 0;
+//       bodyLogo.style.fontSize = '450px'
+//       bodyLogo.style.marginLeft = '10px'
+//       bigimg.style.margin = '0'
 
 
-    }
-  });
+//     }
+//   });
+
+
+  function openLink(){
+    window.open("https://www.linkedin.com/in/raju-prajapati-843b12298?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app","-blank");
+  }
